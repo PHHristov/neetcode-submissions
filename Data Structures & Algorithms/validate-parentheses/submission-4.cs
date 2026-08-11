71c18ec86c3b8158db01cee2c6421f9aad72bbc4
PHHristov <pavlin.hristov101@gmail.com>
2026-08-11 16:23:31 +0300

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public bool IsValid(string s) 
+    {
+        Stack<char> st = new();
+
+        foreach(char c in s)    
+        {
+            if(c == '(' || c == '[' || c == '{') st.Push(c);
+            else 
+            {
+                if(st.Count > 0)
+                {
+                    char p = st.Pop();
+                    if(c == ')' && p != '(') return false;
+                    if(c == '}' && p != '{') return false;
+                    if(c == ']' && p != '[') return false;
+                }
+                else if(st.Count == 0)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return st.Count == 0;
+    }
+}
